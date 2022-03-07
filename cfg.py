1e05f88efbf13839bed56c2e0c81f6c9f13d225e
ChillChewie <99365759+ChillChewie@users.noreply.github.com>
2022-03-07 16:21:01 -0600

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -8,10 +8,6 @@ PASS = "oauth:XXXX" #https://twitchapps.com/tmi/ - sign in with bot channel
 CHAN = "chillchewie" # your main channel - change from chillchewie
 RATE = (100/120) # messages per second
 
-
-
-Banned={}  #dictionary to store banned users
-
 # APIs for calls
 clientid = "XXXXX" #Step 2 #https://dev.twitch.tv/console/apps
 clientsecret = "XXXX" #Step 2
@@ -20,5 +16,6 @@ leagueAPI = "RGAPI-XXXX" #Step 3  #https://developer.riotgames.com/ log in with 
 
 #banned words in chat
 Banhammer = ('list','of','words') #change this list to words that you want banned in your chat
+Banned={}  #dictionary to store chat violators
 
 #make sure to change the social media commands to fit your needs in the bot.py file!!
